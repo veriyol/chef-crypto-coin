f5b3d35a5f51ed39e3fab1678be014e7ca7c4403
Çağatay Yüksel <cagatay@cagatay.co.uk>
2015-11-22 20:44:46 +0100

    Set rpc credentials

--- recipes/litecoin.rb
+++ recipes/litecoin.rb
@@ -1,8 +1,9 @@
 # Download, compile and configure the cryptocoin
 crypto_coin "litecoin" do
-  url    "https://download.litecoin.org/litecoin-0.10.2.2/linux/litecoin-0.10.2.2-linux64.tar.gz"
+  url "https://download.litecoin.org/litecoin-0.10.2.2/linux/litecoin-0.10.2.2-linux64.tar.gz"
+  port 9333
   rpcport 19332
-  rpcpassword   "1234"
+  rpcpassword "1234"
   checksum '136779e717603002f0a3f0da4f48f38274a286171cff10dd68da067ed82c8b26'
 end
 
